9f3ebbd1ff59fa5d73f2e8d0966b11a6d106c892
Gunjan1116 <kumargunjan1116@gmail.com>
2022-11-09 20:03:46 +0530

    added some lower section in home page

---
+++ index.js
@@ -0,0 +1,40 @@
+// All code of home page(index page)
+let data=[
+    {
+        image:"https://i1.wp.com/loseitblog.com/wp-content/uploads/2019/09/Untitled-design-119.png?w=1010&ssl=1"
+        ,des:"Setting Macronutrients Goals: How Many Macros do You Need?"
+    },
+    {
+        image: "https://i2.wp.com/loseitblog.com/wp-content/uploads/2019/02/Quick-tips-to-make-healthier-choices.png?w=1010&ssl=1"
+        ,des:"Quick Tips to Make Healthier Decisions"
+    },
+    {
+        image: "https://i1.wp.com/loseitblog.com/wp-content/uploads/2019/09/Untitled-design-132.png?w=1010&ssl=1"
+        ,des:"12 Protine-Packed Snacks to Curb Hanger Quick"
+    },
+    {
+        image: "https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/01/health-benefits-of-seltzer-water.png?w=1010&ssl=1"
+        ,des:"Is Drinking Seltzer Actually Healthy?" 
+    },
+    {
+        image: "https://i2.wp.com/loseitblog.com/wp-content/uploads/2019/09/Untitled-design-135.png?w=1010&ssl=1"
+        ,des:"Fun 20-Minutes Treadmill Workouts" 
+    },
+    {
+        image: "https://i1.wp.com/loseitblog.com/wp-content/uploads/2019/01/Seven-benefits-of-sleep.png?w=1010&ssl=1"
+        ,des:"Seven Strategies for a Better Night Sleep" 
+    },
+    {
+        image: "https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/01/Intuitive-Eating.png?w=1010&ssl=1"
+        ,des:"The Secret Behind Intuitive Eating" 
+    },
+    {
+        image: "https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/08/Untitled-design-83.png?w=1010&ssl=1"
+        ,des:"5 Ways You're Sabotaging Your Weight Loss" 
+    },
+    {
+        image: "https://i0.wp.com/loseitblog.com/wp-content/uploads/2019/09/Untitled-design-126.png?w=1010&ssl=1"
+        ,des:"10 Tips For Making Healthier Choices When Dining Out" 
+    }]
+
+    
